ce40d802b61f28ab15555044a558c2d1d23c9587
cromatyt <cromatyt@protonmail.com>
2023-05-21 19:44:54 +0200

    remove terraform check in github action

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,11 @@
 output "ec2_test" {
   description = "ID of the EC2 instance"
   value = aws_instance.test1.id
+  sensitive = false
 }
 
 output "instance_public_ip" {
   description = "Public IP address of the EC2 instance"
   value       = aws_instance.test1.public_ip
+  sensitive = false
 }
