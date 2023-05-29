36ce0c368f9139f311a8c652ca12e87f6a758673
cromatyt <cromatyt@protonmail.com>
2023-05-29 22:43:12 +0200

    ecs

--- ec2.tf
+++ ec2.tf
@@ -9,7 +9,7 @@ resource "aws_instance" "test1" {
   key_name                    = var.key_name # for ssh key config
   vpc_security_group_ids      = [aws_security_group.test1_sg.id]
   depends_on                  = [aws_internet_gateway.test1_ig]
-  subnet_id                   = aws_subnet.public_subnets[*].id[count.index]
+  subnet_id                   = aws_subnet.public_subnets.id[count.index]
   associate_public_ip_address = true
 
   tags = {
