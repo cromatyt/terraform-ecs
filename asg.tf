5af1e08a187624897fdd650cf861c424db019b25
cromatyt <cromatyt@protonmail.com>
2023-05-29 23:47:23 +0200

    add asg

--- asg.tf
+++ asg.tf
@@ -59,8 +59,9 @@ resource "aws_launch_template" "ecs_launch_config" {
 }
 
 resource "aws_autoscaling_group" "failure_analysis_ecs_asg" {
+  count                     = length(var.public_subnet_cidrs)
   name_prefix               = "myasg-"
-  vpc_zone_identifier       = aws_subnet.public_subnets[count.index].id
+  vpc_zone_identifier       = [aws_subnet.public_subnets[count.index].id]
   launch_configuration      = {
     id      = aws_launch_template.ecs_launch_config.id
     version = aws_launch_template.ecs_launch_config.latest_version
