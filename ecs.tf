a10a5c6a2774ff2fb3401a32da94488b02ac3f25
cromatyt <cromatyt@protonmail.com>
2023-05-29 23:00:51 +0200

    ecs

--- ecs.tf
+++ ecs.tf
@@ -40,7 +40,7 @@ resource "aws_ecs_task_definition" "task_definition_test1" {
 resource "aws_ecs_service" "ecs_service" {
   name            = "service_test1"
   cluster         = aws_ecs_cluster.ecs_cluster_test1.id
-  task_definition = aws_ecs_task_definition.task_definition_test1.id
+  task_definition = aws_ecs_task_definition.task_definition_test1.arn
   desired_count   = 2
-  launch_type     = "EC2"
+  #launch_type     = "EC2"
 }
