0dd295d36e53b55e9367cd559c6fce1a5eb2baf0
cromatyt <cromatyt@protonmail.com>
2023-05-29 14:38:44 +0200

    typo

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "test1" {
   #count = var.ec2_instance_number
   instance_type               = var.ec2_instance_type
   key_name                    = var.key_name # for ssh key config
-  vpc_security_group_ids      = [aws_security_group.test1-sg.id]
+  vpc_security_group_ids      = [aws_security_group.test1_sg.id]
   depends_on                  = [aws_internet_gateway.test1_ig]
   subnet_id                   = element(aws_subnet.public_subnets[*].id, count.index)
   associate_public_ip_address = true
