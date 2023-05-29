9e449f059d3a58980a5d8c2aff7b2e39cc104e40
cromatyt <cromatyt@protonmail.com>
2023-05-29 14:36:58 +0200

    custom network conf

--- ec2.tf
+++ ec2.tf
@@ -2,10 +2,10 @@ resource "aws_instance" "test1" {
   ami                         = var.ami
   #count = var.ec2_instance_number
   instance_type               = var.ec2_instance_type
-  key_name                    = var.key_name # for ssh config
+  key_name                    = var.key_name # for ssh key config
   vpc_security_group_ids      = [aws_security_group.test1-sg.id]
   depends_on                  = [aws_internet_gateway.test1_ig]
-  subnet_id                   = aws_subnet.public_subnets.id
+  subnet_id                   = element(aws_subnet.public_subnets[*].id, count.index)
   associate_public_ip_address = true
 
   tags = {
