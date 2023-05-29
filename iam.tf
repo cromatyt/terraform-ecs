f3b6326844e55b49b4aa041203acf02144623d8c
cromatyt <cromatyt@protonmail.com>
2023-05-29 23:34:47 +0200

    add asg

--- iam.tf
+++ iam.tf
@@ -22,7 +22,7 @@ resource "aws_iam_role_policy_attachment" "ecs_iam_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
-#resource "aws_iam_instance_profile" "ecs_iam_agent" {
-#  name = "ecs_iam_agent"
-#  role = aws_iam_role.ecs_iam_role.name
-#}
+resource "aws_iam_instance_profile" "ecs_iam_agent" {
+  name = "ecs_iam_agent"
+  role = aws_iam_role.ecs_iam_role.name
+}
