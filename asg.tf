176d663a14dec0a7ce09dd6d7bcccc5050cc6dcc
cromatyt <cromatyt@protonmail.com>
2023-05-30 11:55:06 +0200

    asg

--- asg.tf
+++ asg.tf
@@ -2,15 +2,19 @@
 # Auto scaling group #
 ######################
 
+locals {
+  ecs_agent = templatefile("${path.module}/ecs-agent.sh", { ecs_cluster_name = var.ecs_cluster_name })
+}
+
 resource "aws_launch_template" "ecs_launch_config" {
   name                    = "my-launch-template"
   image_id                = var.ami
   vpc_security_group_ids  = [aws_security_group.test1_sg.id]
-  #user_data               = "#!/bin/bash\necho ECS_CLUSTER=my-cluster >> /etc/ecs/ecs.config"
+  user_data               = base64encode(local.ecs_agent)
   instance_type           = var.ec2_instance_type
 }
 
-resource "aws_autoscaling_group" "failure_analysis_ecs_asg" {
+resource "aws_autoscaling_group" "ecs_asg" {
   count                     = length(var.public_subnet_cidrs)
   name_prefix               = "myasg-"
   vpc_zone_identifier       = [aws_subnet.public_subnets[count.index].id]
@@ -20,9 +24,17 @@ resource "aws_autoscaling_group" "failure_analysis_ecs_asg" {
     version = aws_launch_template.ecs_launch_config.latest_version
   }
 
-  desired_capacity          = 3
-  min_size                  = 3
-  max_size                  = 6
+  desired_capacity          = 1
+  min_size                  = 1
+  max_size                  = 3
   health_check_grace_period = 300
   health_check_type         = "EC2"
+}
+
+resource "aws_autoscaling_group_tag" "ecs_asg_tag" {
+  count = length(var.private_subnet_cidrs)
+
+  tags = {
+    Name = "${var.ecs_cluster_name}-${count.index}"
+  }
 }
