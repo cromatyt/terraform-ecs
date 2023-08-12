0e96b47567e95180cc8bbf210c68a8c205110eb1
cromatyt <cromatyt@protonmail.com>
2023-08-12 23:35:37 +0200

    iam

--- iam.tf
+++ iam.tf
@@ -21,7 +21,7 @@ resource "aws_iam_role" "ecs_iam_role" {
 resource "aws_iam_role_policy_attachment" "ecs_iam_policy" {
   role       = aws_iam_role.ecs_iam_role.name
   # policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-  policy_arn = "arn:aws:iam::354256391653:role/ecsInstanceRole"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceRole"
 }
 
 resource "aws_iam_instance_profile" "ecs_iam_agent" {
