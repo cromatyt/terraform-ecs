db7bc8f55e4da23fa8e16f3572f25294746efee7
cromatyt <cromatyt@protonmail.com>
2023-08-14 02:38:55 +0200

    test

--- lb.tf
+++ lb.tf
@@ -29,14 +29,19 @@ resource "aws_lb_listener" "lb_listener_http_test1" {
   port              = "80"
   protocol          = "HTTP"
 
-  default_action {
-    type = "fixed-response"
+  # default_action {
+  #   type = "fixed-response"
 
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "Access denied"
-      status_code  = "403"
-    }
+  #   fixed_response {
+  #     content_type = "text/plain"
+  #     message_body = "Access denied"
+  #     status_code  = "403"
+  #   }
+  # }
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.lb_target_group_test1.arn
   }
 
   depends_on = [aws_lb.lb_test1]
