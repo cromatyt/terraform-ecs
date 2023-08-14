41480e7bf965d5d0e8ec5ed5f153d554159f05c4
cromatyt <cromatyt@protonmail.com>
2023-08-14 02:52:25 +0200

    test

--- iam.tf
+++ iam.tf
@@ -38,7 +38,7 @@ data "aws_iam_policy_document" "ecs_assume_role" {
 
     principals {
       type        = "Service"
-      identifiers = ["ecs.amazonaws.com"]
+      identifiers = ["ec2.amazonaws.com", "ecs.amazonaws.com"]
     }
   }
 }
@@ -49,5 +49,5 @@ resource "aws_iam_role" "ecs_iam_role" {
 
 resource "aws_iam_role_policy_attachment" "ecs_iam_policy" {
   role       = aws_iam_role.ecs_iam_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceRole"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
 }
