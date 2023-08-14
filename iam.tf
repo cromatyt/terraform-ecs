947bf1b7cbaaddb94a659d97b3b11a826eecd2c9
cromatyt <cromatyt@protonmail.com>
2023-08-14 11:54:51 +0200

    iam test

--- iam.tf
+++ iam.tf
@@ -49,5 +49,5 @@ resource "aws_iam_role" "ecs_iam_role" {
 
 resource "aws_iam_role_policy_attachment" "ecs_iam_policy" {
   role       = aws_iam_role.ecs_iam_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSCodeDeployRoleForECSLimited"
 }
